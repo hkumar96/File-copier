3f0249e6c0a6d8d65b7fcbf3a62e15bb4bd1109c
Hemant Kumar <hemant30nick.com@gmail.com>
2016-01-05 12:24:37 +0530

    Project files added

---
+++ Copier/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Copier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(17, 66);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(130, 23);
+            this.Button4.TabIndex = 9;
+            this.Button4.Text = "Select first folder";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 209);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(447, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Select second folder";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(343, 209);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(234, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(177, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(234, 21);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 261);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Button4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        internal System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        internal System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        internal System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ Copier/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Copier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            string path;
+            string[] files, dirs;
+            folderBrowserDialog1.ShowDialog();
+            path = folderBrowserDialog1.SelectedPath;
+            files = Directory.GetFiles(path+@"\");
+            dirs = Directory.GetDirectories(path + @"\");
+            comboBox1.Items.Clear();
+            foreach (string name in dirs)
+            {
+                comboBox1.Items.Add(Path.GetFileName(name));
+            }
+            foreach (string name in files)
+            {
+                comboBox1.Items.Add(Path.GetFileName(name));
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string path;
+            string[] files, dirs;
+            folderBrowserDialog1.ShowDialog();
+            path = folderBrowserDialog1.SelectedPath;
+            files = Directory.GetFiles(path + @"\");
+            dirs = Directory.GetDirectories(path + @"\");
+            comboBox2.Items.Clear();
+            foreach (string name in dirs)
+            {
+                comboBox2.Items.Add(Path.GetFileName(name));
+            }
+            foreach (string name in files)
+            {
+                comboBox2.Items.Add(Path.GetFileName(name));
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+            for (int i=0;i<comboBox1.Items.Count;i++)
+            {
+                if (!comboBox2.Items.Contains(comboBox1.Items[i].ToString()))
+                    comboBox3.Items.Add(comboBox1.Items[i]);
+            }
+        }
+    }
+}
